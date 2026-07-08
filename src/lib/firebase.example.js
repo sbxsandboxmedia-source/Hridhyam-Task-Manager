24a64e81f76d24d08c9952d03b1b0bbaa60a5304
sbxsandboxmedia-source <sbx.sandboxmedia@gmail.com>
2026-07-08 15:02:46 +0530

    Update firebase.example.js

--- src/lib/firebase.example.js
+++ src/lib/firebase.example.js
@@ -1,1 +1,19 @@
+// Rename to firebase.js and add your Firebase config
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
+const firebaseConfig = {
+  apiKey: 'YOUR_KEY',
+  authDomain: 'YOUR_PROJECT.firebaseapp.com',
+  projectId: 'YOUR_PROJECT',
+  storageBucket: 'YOUR_PROJECT.appspot.com',
+  messagingSenderId: 'YOUR_SENDER_ID',
+  appId: 'YOUR_APP_ID'
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
