b06f5807b5b99d1370821fc6a32a66a49b448941
sbxsandboxmedia-source <sbx.sandboxmedia@gmail.com>
2026-07-09 11:11:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// IMPORTANT for GitHub Pages repo URL:
+// https://USERNAME.github.io/Hridhyam-Task-Manager/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Hridhyam-Task-Manager/'
+});
